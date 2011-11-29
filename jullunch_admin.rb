52cfce0d357e0c16d7c3633f1e255b472573317f
Peter Hellberg <peter@c7.se>
2011-11-29 02:36:44 +0100

    Only require authentication in production

--- jullunch_admin.rb
+++ jullunch_admin.rb
@@ -16,11 +16,6 @@ class JullunchAdmin < Sinatra::Base
   use Rack::Session::Cookie, :key    => 'athega_jullunch',
                              :secret => 'Knowledge is power and true Sith do not share power.'
 
-  use OmniAuth::Strategies::GoogleApps,
-        OpenID::Store::Filesystem.new('/tmp'),
-          :name   => 'athega',
-          :domain => 'athega.se'
-
   #############################################################################
   # Configuration
   #############################################################################
@@ -33,16 +28,24 @@ class JullunchAdmin < Sinatra::Base
   configure :development do
     set :mongodb_uri, 'mongodb://localhost'
     set :mongodb_database, 'athega_jullunch'
+    set :forced_authentication, true
   end
 
   configure :production do
     mongodb_uri = URI.parse(ENV['MONGOLAB_URI'])
     set :mongodb_uri, mongodb_uri.to_s
     set :mongodb_database, mongodb_uri.path.gsub(/^\//, '')
+
+    use OmniAuth::Strategies::GoogleApps,
+        OpenID::Store::Filesystem.new('/tmp'),
+          :name   => 'athega',
+          :domain => 'athega.se'
+
   end
 
   helpers do
     def logged_in?
+      return true unless settings.forced_authentication.nil?
       !session[:current_user_email].nil?
     end
   end
@@ -81,11 +84,61 @@ class JullunchAdmin < Sinatra::Base
   # Application routes
   #############################################################################
 
-  get '/admin' do
-    collections = []
+  get '/admin/?' do
+    redirect to('/admin/guests')
+  end
 
-    @db.collection_names.each { |name| collections << name }
+  get '/admin/guests' do
+    # Guest.new()
 
-    'Jullunch ADMIN!' + session[:current_user_email] + settings.mongodb_database + collections.join(':')
+    haml :'admin/guests/index', locals: {
+      page_title: 'Gäster - Athega Jullunch',
+      guests: Guest.sort([:starts_at, -1]).all
+    }
+  end
+
+  get '/admin/seatings' do
+    haml :'admin/seatings', locals: {
+      page_title: 'Sittningar - Athega Jullunch',
+      seatings: Seating.sort([:starts_at, -1]).all
+    }
+  end
+
+  post '/admin/seatings' do
+    seating = Seating.by_key(params[:key].to_i)
+
+    if seating.nil?
+      seating = Seating.new
+
+      seating.key       = params[:key].to_i
+      seating.title     = params[:title]
+      seating.starts_at = Time.parse(params[:starts_at]).utc
+
+      seating.save
+    end
+
+    redirect to('/admin/seatings')
+  end
+
+  post '/admin/seatings/:key' do
+    seating = Seating.by_key(params[:key].to_i)
+
+    unless seating.nil?
+      if params[:title].empty? || params[:starts_at].empty?
+        seating.delete
+      else
+        seating.title = params[:title]
+        seating.starts_at = Time.parse(params[:starts_at]).utc
+        seating.save
+      end
+    end
+
+    redirect to('/admin/seatings')
+  end
+
+  get '/admin/notifications' do
+    haml :'admin/notifications', locals: {
+      page_title: 'Notifikationer - Athega Jullunch'
+    }
   end
 end
