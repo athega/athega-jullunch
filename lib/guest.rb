c77750834782f77e668447aae5e3a23a8711e107
Peter Hellberg <peter@c7.se>
2011-11-30 11:51:09 +0100

    Assigned CSS classes to each guest in the list.

--- lib/guest.rb
+++ lib/guest.rb
@@ -20,7 +20,16 @@ class Guest
 
   def sitting
     s = Sitting.by_key(sitting_key)
-    s.nil? ? 'Ej valt' : s.title
+
+    if s.nil?
+      'Ej valt'
+    else
+      declined? ? 'Nej' : s.title
+    end
+  end
+
+  def css_classes
+    "guest #{sitting_class} #{invite_class}"
   end
 
   def declined?
@@ -39,5 +48,22 @@ class Guest
   end
 
   after_create :set_default_values
+
+  private
+
+  def sitting_class
+    {
+      0    => 'declined',
+      1130 => 'sitting-1130',
+      1200 => 'sitting-1200',
+      1230 => 'sitting-1230',
+      1300 => 'sitting-1300',
+      1330 => 'sitting-1330'
+    }[sitting_key] || 'no-response'
+  end
+
+  def invite_class
+    invited_manually == true ? 'manual' : 'auto'
+  end
 end
 
