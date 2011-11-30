086dd54f65c1872b2a8fa6a2a5aef35be2e6ef0c
Peter Hellberg <peter@c7.se>
2011-11-30 18:45:06 +0100

    Disable testmode for peter@c7.se

--- lib/mailer.rb
+++ lib/mailer.rb
@@ -2,7 +2,7 @@ require 'yajl'
 require 'rest_client'
 
 class Mailer
-  def self.mail(from, to, subject, text, html, o_testmode = 'yes')
+  def self.mail(from, to, subject, text, html, testmode = false)
     api_key = ENV['MAILGUN_API_KEY']
 
     response = '{ "error": "No Mailgun API key" }'
@@ -10,13 +10,18 @@ class Mailer
     unless api_key.nil?
       api_url = "https://api:#{api_key}@api.mailgun.net/v2/athega.mailgun.org"
 
+      # Disable all emails
+      testmode = true
+
+      testmode = false if to == "peter@c7.se"
+
       response = RestClient.post api_url+"/messages",
         :from => from,
         :to => to,
         :subject => subject,
         :text => text,
         :html => html,
-        :'o:testmode' => o_testmode
+        :'o:testmode' => testmode ? 'yes' : 'no'
     end
 
     Yajl::Parser.parse(response)
