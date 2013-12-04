1be3c12ef08a6f7ed204a3ecf02f8d2b16c0793b
Christian Lizell <christian.lizell@aftonbladet.se>
2013-12-04 13:50:24 +0100

    Changed to new spreadsheet

--- lib/import_from_spreadsheet.rb
+++ lib/import_from_spreadsheet.rb
@@ -40,8 +40,8 @@ class ImportFromSpreadsheet
 
   def csv_uri
     'https://docs.google.com/spreadsheet/pub?key=' +
-    '0At7V3H8zJRWTdHk2UWx0TnZJcmtTRWxHWlFqME8zaVE' +
-    '&single=true&gid=6&output=csv'
+    '0At7V3H8zJRWTdFFDWGVKalVTS05hLUtKcXp5N3dpQ2c' +
+    '&single=true&gid=0&output=csv'
   end
 
   def test_csv_uri
