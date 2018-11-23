828f839e5418722b539ee8be83e5aad3e9d57c69
Christian Lizell <christian.lizell@athega.se>
2018-11-23 15:20:45 +0100

    fix: mongoid query

--- lib/notification.rb
+++ lib/notification.rb
@@ -77,7 +77,7 @@ class Notification
 
     sent_count = 0
 
-    Guest.not_thanked_yet.arrived.where(company: { _ne: 'Vänner & Familj' }).limit(30).each do |g|
+    Guest.not_thanked_yet.arrived.where(:company.ne => 'Vänner & Familj').limit(30).each do |g|
       html = renderer.call mulled_wine: g.mulled_wine,
                            food:        g.food,
                            coffee:      g.coffee,
